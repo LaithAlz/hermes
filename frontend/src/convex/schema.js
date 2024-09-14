8408d33d189723ac88e4fad23eac950054504c23
Allaith Ahmed Alzoubi <allaith.alzoubi@mail.utoronto.ca>
2024-09-14 15:00:31 -0400

    restarting

--- frontend/src/convex/schema.js
+++
@@ -1,9 +0,0 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
-
-export default defineSchema({
-  conversations: defineTable({
-    content: v.string(),
-    user: v.int64(),
-  }),
-});
