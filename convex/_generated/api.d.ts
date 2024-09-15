4443cf3a5f15b6f18ade9287aedb6c3fe8f9e0ce
Allaith Ahmed Alzoubi <allaith.alzoubi@mail.utoronto.ca>
2024-09-14 22:27:10 -0400

    merging

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,7 +15,6 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as conversations from "../conversations.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,9 +24,7 @@ import type * as conversations from "../conversations.js";
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{
-  conversations: typeof conversations;
-}>;
+declare const fullApi: ApiFromModules<{}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
